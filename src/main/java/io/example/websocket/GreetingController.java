4da1ece11c7527e85e9417ba8470a858acc61160
jinhyun <free1330@gmail.com>
2015-05-11 17:53:16 +0900

    feature(websocket): push data

--- src/main/java/io/example/websocket/GreetingController.java
+++ src/main/java/io/example/websocket/GreetingController.java
@@ -1,8 +1,13 @@
 package io.example.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
@@ -11,5 +16,21 @@ public class GreetingController {
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(3000); // simulated delay
         return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+    @RequestMapping("/webSocket")
+    public ModelAndView webSocket() {
+        return new ModelAndView("webSocket");
+    }
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @RequestMapping("/webSocket/pushData")
+    @ResponseBody
+    public String webSocketPushData() throws Exception {
+        String text = "Goooooood";
+        this.template.convertAndSend("/topic/greetings", new Greeting(text));
+        return "input Data: " + text;
     }
 }
